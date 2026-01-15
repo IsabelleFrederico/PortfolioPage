b5a1158385f4bf2e20c0113f21cda38c75c495d6
IsabelleFrederico <isa.frederico@gmail.com>
2026-01-15 23:12:38 +0100

    screen mp4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Suspense } from "react";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 1, 5], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
-      <Experience />
+      <Suspense fallback={null}>
+        <Experience />
+      </Suspense>
     </Canvas>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,20 @@
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { Office } from "./Office";
+import { CatMia } from "./CatMia";
+import { Suspense } from "react";
 
 export const Experience = () => {
   return (
     <>
+      <ambientLight intensity={1} />
       <OrbitControls />
-      <group position-y={-1}>
-        <Office />
+      <group position-y={-0.5}>
+        <Suspense fallback={null}>
+          <Office />
+        </Suspense>
         <Avatar />
-        <ambientLight intensity={1} />
+        <CatMia />
       </group>
     </>
   );
